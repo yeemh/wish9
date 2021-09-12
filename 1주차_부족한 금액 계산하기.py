f83b4b5e86e567f9609d6a2c205da22f438ec871
MINHO LEE <51910605+yeemh@users.noreply.github.com>
2021-09-12 20:52:58 +0900

    Delete 1주차_부족한 금액 계산하기.py

--- 1주차_부족한 금액 계산하기.py
+++
@@ -1,8 +0,0 @@
-def solution(price, money, count):    
-    for i in range(count):
-        money -= price * (i+1)
-        
-    if money >= 0:
-        return 0
-    else:
-        return abs(money)
